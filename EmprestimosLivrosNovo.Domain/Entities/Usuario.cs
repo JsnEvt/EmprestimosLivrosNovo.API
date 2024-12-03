21bfea2c3052a8ff502ab49fe39d2b5ca1c2e2c7
Jason Santos <110934550+JsnEvt@users.noreply.github.com>
2024-12-03 17:21:12 -0300

    Services, Interfaces and DTO/Usuario

--- EmprestimosLivrosNovo.Domain/Entities/Usuario.cs
+++ EmprestimosLivrosNovo.Domain/Entities/Usuario.cs
@@ -7,13 +7,13 @@ namespace EmprestimosLivrosNovo.Domain.Entities;
 
 public partial class Usuario
 {
-    public int Id { get; set; }
+    public int Id { get; private set; }
 
-    public string Nome { get; set; }
+    public string Nome { get; private set; }
 
-    public string Email { get; set; }
+    public string Email { get; private set; }
 
-    public byte[] PasswordHash { get; set; }
+    public byte[] PasswordHash { get; private set; }
 
-    public byte[] PasswordSalt { get; set; }
+    public byte[] PasswordSalt { get; private set; }
 }
