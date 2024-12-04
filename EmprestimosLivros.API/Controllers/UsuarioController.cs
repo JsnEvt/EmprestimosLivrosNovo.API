8a4e6af79544e21b6742020314af70c98dbdef30
Jason Santos <110934550+JsnEvt@users.noreply.github.com>
2024-12-04 15:53:07 -0300

    Adding Authenticate class/services/token

---
+++ EmprestimosLivros.API/Controllers/UsuarioController.cs
@@ -0,0 +1,17 @@
+﻿using EmprestimosLivros.API.Models;
+using EmprestimosLivrosNovo.Application.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmprestimosLivros.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsuarioController : Controller
+    {
+        [HttpPost("register")]
+        public async Task<ActionResult<UserToken>> Incluir(UsuarioDTO usuarioDTO)
+        {
+            return View();
+        }
+    }
+}
---
+++ EmprestimosLivrosNovo.Domain/Account/IAuthenticate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmprestimosLivrosNovo.Domain.Account
+{
+    public interface IAuthenticate
+    {
+        Task<bool> AuthenticateAsync(string email, string senha);
+        Task<bool> UserExists(string email);
+        public string GenerateToken(int id, string email);
+    }
+}
